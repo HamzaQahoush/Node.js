827f98a2ebb04cb48850e71bb959ed4417becffc
HamzaQahoush <hamza.qah@gmail.com>
2021-09-12 08:27:57 +0300

    assigment 3 finised

---
+++ assigment3/routes/user.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const path = require("path");
+
+const router = express.Router();
+router.get("/user", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "/..", "views", "users.html"));
+});
+module.exports = router;
